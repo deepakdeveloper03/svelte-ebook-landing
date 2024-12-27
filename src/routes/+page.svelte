23691babe912240d21053ef343139ab10462c0f9
deepak <deepakdeveloper03@gmail.com>
2024-12-27 15:47:21 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	// import HeroSection from "$lib/components/HeroSection.svelte";
+    import {HeroSection,ChapterPreview,AuthorSection,FaqSection} from "$components";
+	
+</script>
+<HeroSection>
+
+</HeroSection>
+
+<ChapterPreview></ChapterPreview>
+
+<AuthorSection></AuthorSection>
+
+<FaqSection></FaqSection>
+
